f03ae927f6e63fac42d3af5e564ab3125d23212d
Joe Roth <jroth@gmail.com>
2017-07-24 17:05:57 -0400

    Derive address

--- addr/addr.go
+++ addr/addr.go
@@ -1,18 +1,46 @@
 package addr
 
+import (
+	"encoding/hex"
+	"fmt"
+
+	"github.com/ethereum/go-ethereum/crypto"
+)
+
 type Account struct {
-	PrivateKey []byte
+	privateKey []byte
 }
 
 // NewAddress returns an address given hex encoded private key.
-func NewAccount(pk string) Account {
+func NewAccount(pk string) (Account, error) {
+	priv, err := hex.DecodeString("cb4aab9577130f5c4622f355e5c6c3cad2661518ac968c34e4f14a9fde071bfd")
+	if err != nil {
+		return Account{}, err
+	}
 
+	if len(priv) != 32 {
+		return Account{}, fmt.Errorf("private key not 32 bytes")
+	}
+
+	return Account{
+		privateKey: priv,
+	}, nil
 }
 
-func (a Accout) Address() string {
+func (a Account) Address() string {
+	x, y := crypto.S256().ScalarBaseMult(a.privateKey)
 
+	// Create ECDSA public key (64 bytes)
+	pub := append(x.Bytes(), y.Bytes()...)
+
+	// Keccak-256 hash of pub key (32 bytes)
+	// ehtereum uses a special Keccak256 configuration with dsbyte: 0x01
+	c := crypto.Keccak256(pub)
+
+	// Take last 20 bytes, and prepend '0x'
+	return fmt.Sprintf("0x%x", c[12:])
 }
 
 func (a Account) Sign(d []byte) []byte {
-
+	return []byte{}
 }
---
+++ addr/addr_test.go
@@ -0,0 +1,23 @@
+package addr
+
+import "testing"
+
+func TestAddress(t *testing.T) {
+	var tests = []struct {
+		pk, address string
+	}{
+		{"cb4aab9577130f5c4622f355e5c6c3cad2661518ac968c34e4f14a9fde071bfd", "0x9f872283587d655cba2b13f313511aea353903d9"},
+	}
+
+	for _, test := range tests {
+		acct, err := NewAccount(test.pk)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if ad := acct.Address(); ad != test.address {
+			t.Logf("Expected: %T, received: %T", test.address, ad)
+			t.Logf("Expected: %+v, received: %+v", test.address, ad)
+			t.Fatalf("Expected: %s, received: %s", test.address, ad)
+		}
+	}
+}
