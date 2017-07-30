d177e0e4ef45363f526cc6bf8c9ddfcbe54fe74f
Joe Roth <jroth@gmail.com>
2017-07-30 14:30:21 -0400

    Account sign and recover

--- accnt/accnt.go
+++ accnt/accnt.go
@@ -4,13 +4,10 @@ import (
 	"crypto/ecdsa"
 	"encoding/hex"
 	"fmt"
+	"math/big"
 
 	"github.com/ethereum/go-ethereum/crypto"
 )
-
-type Account struct {
-	privateKey []byte
-}
 
 type Private struct {
 	*ecdsa.PrivateKey
@@ -18,6 +15,14 @@ type Private struct {
 }
 
 type Public ecdsa.PublicKey
+
+// Compressed signature allows for the recovery of a public key given the hash
+// of a message.
+type Signature struct {
+	R *big.Int
+	S *big.Int
+	V bool // false == 0, true == 1
+}
 
 // NewAddress returns an address given hex encoded private key.
 func NewAccount(pk string) (Private, error) {
@@ -35,6 +40,39 @@ func NewAccount(pk string) (Private, error) {
 		PrivateKey: privk,
 		Public:     Public(privk.PublicKey),
 	}, nil
+}
+
+func (p Private) Sign(in []byte) (Signature, error) {
+	// The produced signature is in the [R || S || V] format where V is 0 or 1.
+	sig, err := crypto.Sign(in, p.PrivateKey)
+	if err != nil {
+		return Signature{}, err
+	}
+
+	return Signature{
+		R: new(big.Int).SetBytes(sig[:32]),
+		S: new(big.Int).SetBytes(sig[32:64]),
+		V: func(b byte) bool {
+			return b == byte(1)
+		}(sig[64]),
+	}, nil
+}
+
+func Recover(in []byte, s Signature) (Public, error) {
+	sig := append(s.R.Bytes(), s.S.Bytes()...)
+	sig = append(sig, func(b bool) byte {
+		if b {
+			return 1
+		}
+		return 0
+	}(s.V))
+
+	pubk, err := crypto.SigToPub(in, sig)
+	if err != nil {
+		return Public{}, err
+	}
+
+	return Public(*pubk), nil
 }
 
 func (p Public) Address() string {
--- accnt/accnt_test.go
+++ accnt/accnt_test.go
@@ -1,6 +1,10 @@
 package accnt
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/ethereum/go-ethereum/crypto"
+)
 
 func TestAddress(t *testing.T) {
 	var tests = []struct {
@@ -17,5 +21,27 @@ func TestAddress(t *testing.T) {
 		if ad := acct.Address(); ad != test.address {
 			t.Fatalf("Expected: %s, received: %s", test.address, ad)
 		}
+	}
+}
+
+func TestSignRecover(t *testing.T) {
+	priv, err := NewAccount("cb4aab9577130f5c4622f355e5c6c3cad2661518ac968c34e4f14a9fde071bfd")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	msg := crypto.Keccak256([]byte("test"))
+	sig, err := priv.Sign(msg)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	pub, err := Recover(msg, sig)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if priv.Address() != pub.Address() {
+		t.Fatalf("Expected: %s, received: %s", priv.Address(), pub.Address())
 	}
 }
